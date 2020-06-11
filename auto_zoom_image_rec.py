116cc4b9b409ea5691300b43976f44ddccbaeac4
Dom Gasperini <38026244+dominic-g@users.noreply.github.com>
2020-06-11 02:41:34 -0400

    Add files via upload

--- auto_zoom_image_rec.py
+++ auto_zoom_image_rec.py
@@ -4,7 +4,6 @@ import pyautogui
 import time
 import pyperclip
 
-
 # getting meeting id
 meeting_ID = input('meeting id / class name / clipboard: ')
 meeting_name = 'zoom'
@@ -15,112 +14,106 @@ if meeting_ID == 'calc':
     meeting_ID = '951-444-09514'
     meeting_name = 'calculus'
 
-
 # getting password
 is_password = input('is there a password to this meeting (y / n)?: ')
 password = ''
 if is_password == 'y':
     password = input('enter the password to the meeting: ')
 
-
 # determine os
 os = input('which operating system are you using (windows / mac)?: ')
-
 
 # automation
 time.sleep(1)
 
-
 # search for zoom
 if os == 'mac':
     pyautogui.hotkey('command', 'space')
-    time.sleep(0.5)
+    time.sleep(0.75)
 if os == 'windows':
     pyautogui.hotkey('win')
-    time.sleep(0.5)
-
+    time.sleep(0.75)
 
 # type zoom
-pyautogui.typewrite('Zoom', interval=0.15)
-time.sleep(0.5)
-
+pyautogui.typewrite('Zoom', interval=0.05)
+time.sleep(1)
 
 # open zoom
 pyautogui.hotkey('enter')
-time.sleep(2)
-
 
 # join a meeting button
-pyautogui.screenshot(region=(450, 240, 300, 300))
-x, y = pyautogui.locateCenterOnScreen('join_button.png')
-pyautogui.click(x, y)
-time.sleep(1)
-
+while True:
+    pyautogui.screenshot(region=(360, 80, 1200, 900))
+    join_button = pyautogui.locateCenterOnScreen('join_button.png')
+    if join_button is not None:
+        x, y = pyautogui.locateCenterOnScreen('join_button.png')
+        pyautogui.click(x, y)
+        time.sleep(1)
+        break
 
 # enter meeting id
 pyautogui.typewrite(meeting_ID, interval=0.05)
 
-
 # turn off camera pre join
 if is_password == 'n':
-    pyautogui.screenshot(region=(450, 240, 500, 500))
+    pyautogui.screenshot(region=(700, 280, 300, 300))
     x, y = pyautogui.locateCenterOnScreen('pre_join_cam_off.png')
     time.sleep(1)
 
-
 # join meeting
-pyautogui.screenshot(region=(450, 240, 300, 300))
+pyautogui.screenshot(region=(700, 280, 300, 300))
 x, y = pyautogui.locateCenterOnScreen('join_meeting_button.png')
 pyautogui.click(x, y)
-time.sleep(1.5)
-
 
 # check if joined
-pyautogui.screenshot()
-in_meeting = pyautogui.locateOnScreen('leave_button.png')
-if in_meeting:
-    print('meeting detected')
-    print('welcome to your {} meeting'.format(meeting_name))
-    exit()
-if not in_meeting:
-    print('no meeting detected yet')
-
+while True:
+    pyautogui.screenshot()
+    in_meeting = pyautogui.locateOnScreen('leave_button.png')
+    if in_meeting:
+        print('meeting detected')
+        print('welcome to your {} meeting'.format(meeting_name))
+        exit()
+    if in_meeting is None:
+        print('no meeting detected yet')
+        break
 
 # entering password
 if is_password == 'y':
-    pyautogui.typewrite(password)
-
+    pyautogui.typewrite(password, interval=0.05)
 
 # join with password
 if is_password == 'y':
     x, y = pyautogui.locateCenterOnScreen('join_with_password_button.png')
     pyautogui.click(x, y)
-    time.sleep(3)
-
 
 # join with camera prompt
-pyautogui.screenshot(region=(450, 240, 500, 500))
-x, y = pyautogui.locateCenterOnScreen('join_without_video_button.png')
-pyautogui.click(x, y)
-
+while True:
+    pyautogui.screenshot()
+    join_without_video_button = pyautogui.locateCenterOnScreen('join_without_video_button.png')
+    if join_without_video_button is not None:
+        pyautogui.screenshot(region=(360, 80, 1200, 900))
+        x, y = pyautogui.locateCenterOnScreen('join_without_video_button.png')
+        pyautogui.click(x, y)
+        break
 
 # microphone
-pyautogui.screenshot()
-ready = pyautogui.locateCenterOnScreen('unmuted_microphone.png')
-while not ready:
+while True:
     pyautogui.screenshot()
-    if pyautogui.locateCenterOnScreen('unmuted_microphone.png'):
+    unmuted_microphone = pyautogui.locateCenterOnScreen('unmuted_microphone.png')
+    if unmuted_microphone is not None:
         x, y = pyautogui.locateCenterOnScreen('unmuted_microphone.png')
         pyautogui.click(x, y)
         break
 
-
 # full screen
-pyautogui.moveRel(50, 0, duration=1)
-pyautogui.screenshot()
-x, y = pyautogui.locateCenterOnScreen('full_screen_button.png')
-pyautogui.click(x, y)
-
+while True:
+    pyautogui.moveRel(50, 0, duration=1)
+    pyautogui.screenshot()
+    full_screen_button = pyautogui.locateCenterOnScreen('full_screen_button.png')
+    if full_screen_button is not None:
+        x, y = pyautogui.locateCenterOnScreen('full_screen_button.png')
+        pyautogui.click(x, y)
+        break
 
 # all done message for password
 print('welcome to your {} meeting'.format(meeting_name))
